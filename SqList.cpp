a26f59368f49bc8471671ad2b27bc8e50cce00c1
Iridescenttttt <ljx20011211@gmail.com>
2023-03-15 16:28:02 +0800

    SqList

--- SqList.cpp
+++ SqList.cpp
@@ -3,103 +3,126 @@ using namespace std;
 
 #define InitSize 10
 
-class SqList {
-public:
+struct SqList {
     int *data;
     int MaxSize;
     int length;
-
-    /**
-     * @brief Init the list.
-     * 
-     */
-    void InitList() {
-        data = new int[InitSize];
-        MaxSize = InitSize;
-        length = 0;
-    }
-
-    /**
-     * @brief Increase the list length.
-     * 
-     * @param size increase size.
-     */
-    void IncreaseList(int size) {
-        int *p = data;
-        data = new int[InitSize + size];
-        for(int i = 0; i < length; i++){
-            data[i] = p[i];
-        }
-        MaxSize += size;
-        delete []p;
-    }
-
-    /**
-     * @brief Insert element to list.
-     * 
-     * @param pos insert position
-     * @param e element
-     */
-    void ListInsert(int pos, int e) {
-        if(pos < 1 || pos > length + 1)cout<<"Please input the right postion!"<<endl;
-        if(length == MaxSize) IncreaseList(5);
-        for(int i = length + 1; i >= pos; i--){
-            data[i] = data[i - 1];
-        }
-        data[pos - 1] = e;
-        length++;
-    }
-
-    /**
-     * @brief Delete the element at the corresponding position.
-     * 
-     * @param pos position
-     * @return int deleted element
-     */
-    int ListDelete(int pos) {
-        if(pos < 1 || pos > length)cout<<"Please input the right postion!"<<endl;
-        int ret = data[pos - 1];
-        for(int i = pos; i < length; i++){
-            data[i - 1] = data[i];
-        }
-        length--;
-        return ret;
-    }
-
-    /**
-     * @brief Get the Elem object.
-     * 
-     * @param pos position
-     * @return int element
-     */
-    int GetElem(int pos) {
-        if(pos < 1 || pos > length)cout<<"Please input the right postion!"<<endl;
-        return data[pos - 1];
-    }
-
-    /**
-     * @brief Find the element position
-     * 
-     * @param val value
-     * @return int position
-     */
-    int LocateElem(int val) {
-        for(int i = 0; i < length; i++){
-            if(data[i] == val)return i;
-        }
-        return -1;
-    }
 };
 
+/**
+ * @brief Init the list.
+ * 
+ */
+void InitList(SqList &L) {
+    L.data = new int[InitSize];
+    L.MaxSize = InitSize;
+    L.length = 0;
+}
+
+/**
+ * @brief Increase the list length.
+ * 
+ * @param size increase size.
+ */
+void IncreaseList(SqList &L, int size) {
+    int *p = L.data;
+    L.data = new int[InitSize + size];
+    for(int i = 0; i < L.length; i++){
+        L.data[i] = p[i];
+    }
+    L.MaxSize += size;
+    delete []p;
+}
+
+/**
+ * @brief Insert element to list.
+ * 
+ * @param pos insert position
+ * @param e element
+ */
+void ListInsert(SqList &L, int pos, int e) {
+    if(pos < 1 || pos > L.length + 1)cout<<"Please input the right postion!"<<endl;
+    if(L.length == L.MaxSize) IncreaseList(L, 5);
+    for(int i = L.length + 1; i >= pos; i--){
+        L.data[i] = L.data[i - 1];
+    }
+    L.data[pos - 1] = e;
+    L.length++;
+}
+
+/**
+ * @brief Delete the element at the corresponding position.
+ * 
+ * @param pos position
+ * @return int deleted element
+ */
+int ListDelete(SqList &L, int pos) {
+    if(pos < 1 || pos > L.length)cout<<"Please input the right postion!"<<endl;
+    int ret = L.data[pos - 1];
+    for(int i = pos; i < L.length; i++){
+        L.data[i - 1] = L.data[i];
+    }
+    L.length--;
+    return ret;
+}
+
+/**
+ * @brief Get the Elem object.
+ * 
+ * @param pos position
+ * @return int element
+ */
+int GetElem(SqList &L, int pos) {
+    if(pos < 1 || pos > L.length)cout<<"Please input the right postion!"<<endl;
+    return L.data[pos - 1];
+}
+
+/**
+ * @brief Find the element position
+ * 
+ * @param val value
+ * @return int position
+ */
+int LocateElem(SqList L, int val) {
+    for(int i = 0; i < L.length; i++){
+        if(L.data[i] == val)return i + 1;
+    }
+    return -1;
+}
+
+ostream& operator<<(ostream &out, SqList L){
+    for(int i = 0; i < L.length; i++){
+        out<<L.data[i]<<" ";
+    }
+    return out;
+}
 
 int main() {
     SqList L;
-    L.InitList();
-    for(int i = 0; i < 10; i++) {
+    InitList(L);
+    for(int i = 0; i < 9; i++) {
         L.data[i] = i;
         L.length++;
     }
-    int e = L.GetElem(1);
-    cout<<e<<endl;
+    cout<<L<<endl;
+
+    // test increase
+    IncreaseList(L, 5);
+    cout<<L.MaxSize<<endl;
+
+    // test insert
+    ListInsert(L, 5, 9);
+    cout<<L<<endl;
+
+    // test delete
+    ListDelete(L, 1);
+    cout<<L<<endl;
+
+    // test getelem
+    int val = GetElem(L, 1);
+    cout<<val<<endl;
+
+    // test locate
+    cout<<LocateElem(L, 9)<<endl;
     return 0;
 }
